9de2e7b45bd69e57c18a82a548abe22ae981773f
JateX <grisaia@inbox.ru>
2023-03-04 09:43:15 +0300

    Task_2

--- Task_2/Figures/RightTriangle.cpp
+++ Task_2/Figures/RightTriangle.cpp
@@ -1,7 +1,7 @@
 ﻿#include "RightTriangle.hpp"
 #include "CatchError.hpp"
 
-RightTriangle::RightTriangle() : RightTriangle(10, 20, 30, 30, 60, "Прямоугольный треугольник") {}
+RightTriangle::RightTriangle() : RightTriangle(10, 20, 30, 40, 60, "Прямоугольный треугольник") {}
 
 RightTriangle::RightTriangle(int a, int b, int c, int A, int B, std::string _name) : Triangle(a, b, c, A, B, 80, _name) {
     if (RightTriangle::get_corner_c() != 90) throw CatchError("Угол 'c' не равен 90");
